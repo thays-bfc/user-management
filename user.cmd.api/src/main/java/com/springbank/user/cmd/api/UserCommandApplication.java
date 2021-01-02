41572d87fca67adee3076fd3a5a52c815eaf4861
Thays Barbara <thaysbarbara@hotmail.com>
2021-01-02 13:45:49 -0300

    First commit

---
+++ user.cmd.api/src/main/java/com/springbank/user/cmd/api/UserCommandApplication.java
@@ -0,0 +1,13 @@
+package com.springbank.user.cmd.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserCommandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserCommandApplication.class, args);
+	}
+
+}
